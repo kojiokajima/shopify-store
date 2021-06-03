bd2be2ac0dbd87d70e8474561b3a101c82a719c4
Koji Okajima <koji970124@gmail.com>
2021-06-03 09:22:05 -0700

    init commit

---
+++ pages/product/[productId].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+const ProductPage = () => {
+    const router = useRouter()
+    const { productId } = router.query
+    return (
+        <div>
+            hi this product page {productId}
+        </div>
+    )
+}
+export default ProductPage
